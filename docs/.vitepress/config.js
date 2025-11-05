427cd87849f3f95166df1f511341242d785d99bb
Timeless小帅 <1250076672@qq.com>
2025-11-05 22:46:24 +0800

    ipc

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+export default {
+    // 应用级配置选项
+    lang: 'zh-cn',
+    title: 'VitePress',
+    description: 'Vite & Vue powered static site generator.',
+    themeConfig: {
+        footer: {
+            message: 'ICP备案号：<a href="https://beian.miit.gov.cn" target="_blank">冀ICP备2025104367号</a>',
+            copyright: 'Copyright © 2019-2025 阿帅正传'
+        }
+    }
+}
