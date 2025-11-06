e947282aa311275820a97dcf630ea26596630507
Timeless小帅 <1018008088@qq.com>
2025-11-06 13:06:49 +0800

    文章

--- run_prod.sh
+++ run_prod.sh
@@ -5,6 +5,7 @@ npm install
 echo "run docs:build"
 npm run docs:build
 chown -R www:www ./
+echo "npm run docs:preview"
 ps -ef | grep "npm run docs:preview" | grep -v grep | awk '{print $2}' | xargs kill -9
 # 以 www 身份后台运行，忽略挂断信号，输出重定向到日志
 nohup sudo -u www npm run docs:preview > /www/wwwroot/xsinfo/preview.log 2>&1 &
