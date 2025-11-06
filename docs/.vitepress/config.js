7255ee6441999cef3865055285c5c36bbb7abc18
Timeless小帅 <1250076672@qq.com>
2025-11-07 00:03:21 +0800

    关于

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,8 +6,15 @@ export default {
     themeConfig: {
         footer: {
             message: '<a href="https://beian.miit.gov.cn" target="_blank">冀ICP备2025104367号</a>/<a href="https://beian.mps.gov.cn/#/query/webSearch" target="_blank">京公网安备11011502038830号</a>',
-            copyright: 'Copyright © 2019-2025 阿帅正传'
+            copyright: 'Copyright ©2025 阿帅正传 版权所有 | 转载请注明出处'
         },
+        nav: [
+            { text: 'Linux', link: '/linux/linux' },
+            { text: '关于', link: '/about' },
+            // { text: '瑞芯微系列', link: '/config' },
+            // { text: '六轴机械臂', link: '/config' },
+            { text: '文档源码', link: 'https://gitee.com/song_kang_shuai/xsinfo' }
+        ],
         sidebar: {
             // 当用户位于 `guide` 目录时，会显示此侧边栏
             '/linux/': [
--- docs/index.md
+++ docs/index.md
@@ -15,12 +15,12 @@ hero:
 #      link: /linux
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Linux笔记
+    details: 接下来一段时间会持续记录Linux相关笔记，主要是以ubuntu22.04操作系统为主
+  - title: 瑞芯微系列
+    details: 目前再学习瑞芯微系列的开发版，目前围绕RK3506与RK3566系列
+  - title: 六轴机械臂
+    details: 还会不定时更新六轴机械臂相关的笔记，也在学习中。基础操作、欧拉角、具身智能
 
 
 ---
