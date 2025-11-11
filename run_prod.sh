30838bcf38adcd9d0cf6458d315bdf44c38bfef2
Timeless小帅 <1018008088@qq.com>
2025-11-11 10:29:32 +0800

    文档更新时间 Github 上编辑该文档

--- run_prod.sh
+++ run_prod.sh
@@ -11,6 +11,9 @@ echo "run docs:build"
 npm run docs:build
 chown -R www:www ./
 echo "npm run docs:preview"
+
+# 杀掉之前运行的进程
 ps -ef | grep "npm run docs:preview" | grep -v grep | awk '{print $2}' | xargs kill -9
+
 # 以 www 身份后台运行，忽略挂断信号，输出重定向到日志
 nohup sudo -u www npm run docs:preview > /www/wwwroot/xsinfo/preview.log 2>&1 &
